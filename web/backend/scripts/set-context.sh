4b62ab407a83d45f05f15f3ea2725427d7d9bd1d
AndiMajore <andi.majore@googlemail.com>
2021-03-01 17:51:32 +0100

    docker with backend tomcat running

--- web/backend/scripts/set-context.sh
+++ web/backend/scripts/set-context.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sed -i "s/Context path=\"backend\"/Context path=/\"$CONTEXT_PATH\"/g" $1
+name=$(echo "$CONTEXT_PATH" | tr [/] [#])
+if [ ${#name} -ge 2 ]; then name=${name:1:${#name}} ; else name="ROOT"; fi
+echo $name
+sed -i "s|Context path=\"/backend|Context path=\"$CONTEXT_PATH|g" $1
+file=$(dirname $1)
+mv $1 "$file/$name.xml"
