8fac588c3753302ca5244c95ff9fa21a13c63e44
AndiMajore <andi.majore@googlemail.com>
2021-03-07 02:21:05 +0100

    docker fixed

--- web/frontend/src/Config.js
+++ web/frontend/src/Config.js
@@ -1,5 +1,4 @@
-
-export const HOST = process.env.NODE_ENV === "development" ?"localhost": "localhost"
-export const BACKEND_PORT = process.env.NODE_ENV === "development" ? "8080" : "8090"
-export const HOST_URL =  "http://"+ (process.env.NODE_ENV ==="development" ? "localhost:8080" : "localhost:8090")
-export const SOCKET_URL = "ws://"+(process.env.NODE_ENV ==="development" ? "localhost:8090" : "localhost:8090")
+const HOST = (process.env.NODE_ENV === "development" ? (process.env.FRONTEND_HOST_URL ? process.env.FRONTEND_HOST_URL : "localhost") : "localhost")
+export const BACKEND_PORT = (process.env.NODE_ENV === "development" ? "8080" : "8090")
+export const HOST_URL = "http://" + HOST + ":" + BACKEND_PORT
+export const SOCKET_URL = "ws://" + HOST + ":8090"
