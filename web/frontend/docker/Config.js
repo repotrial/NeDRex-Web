912cf80ab1a6685d86881f761edb7340eaa8dcba
AndiMajore <andi.majore@googlemail.com>
2021-03-16 19:32:53 +0100

    adjusted docker-scripts

--- web/frontend/docker/Config.js
+++ web/frontend/docker/Config.js
@@ -1,10 +1,10 @@
 //const HOST = "www.exbio.wzw.tum.de"
-// const PROTOCOL = "https"
+//const PROTOCOL = "https"
 const HOST = "exbio.localhost.com"
 // const HOST = "localhost"
 const PROTOCOL = "http"
 export const CONTEXT_PATH="/nedrex_backend"
-export const BACKEND_PORT =  "8090"
+export const BACKEND_PORT =  "8034"
 export const HOST_URL = PROTOCOL+"://" + HOST + ":" + BACKEND_PORT
-export const SOCKET_URL = "ws://" + HOST + ":8090"
+export const SOCKET_URL = "ws://" + HOST + ":"+BACKEND_PORT
 
