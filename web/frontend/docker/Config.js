a9c1538ecdbd9a49c252c91ad3c0915af488ebf8
AndiMajore <andi.majore@googlemail.com>
2021-03-17 12:07:33 +0100

    adjusted container names

--- web/frontend/docker/Config.js
+++ web/frontend/docker/Config.js
@@ -1,8 +1,8 @@
-//const HOST = "www.exbio.wzw.tum.de"
-//const PROTOCOL = "https"
-const HOST = "exbio.localhost.com"
+const HOST = "www.exbio.wzw.tum.de"
+const PROTOCOL = "https"
+//const HOST = "exbio.localhost.com"
 // const HOST = "localhost"
-const PROTOCOL = "http"
+//const PROTOCOL = "http"
 export const CONTEXT_PATH="/nedrex_backend"
 export const BACKEND_PORT =  "8034"
 export const HOST_URL = PROTOCOL+"://" + HOST + ":" + BACKEND_PORT
