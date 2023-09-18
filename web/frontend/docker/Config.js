2de76f036a53cdfe764e382c4c828507289a3f07
AndiMajore <andi.majore@googlemail.com>
2023-09-18 16:05:07 +0200

    updated for new servers

--- web/frontend/docker/Config.js
+++ web/frontend/docker/Config.js
@@ -1,12 +1,11 @@
 
-const HOST = "localhost"
-const PROTOCOL = "http"
-
-export const CONTEXT_PATH="/nedrex_backend"
-export const BACKEND_PORT =  "8034"
-export const HOST_URL = PROTOCOL+"://" + HOST + ":" + BACKEND_PORT
-export const SOCKET_URL = PROTOCOL+"://" + HOST + ":"+BACKEND_PORT
+const HOST = "api.web.nedrex.net"
+const PROTOCOL = "https"
+export const CONTEXT_PATH=""
+export const HOST_URL = PROTOCOL+"://" + HOST
+export const SOCKET_URL = PROTOCOL+"://" + HOST
 export const PATH_PREFIX= ""
 export const STATIC_PATH="/static/"
 
 export const NEDREX_API=PROTOCOL+"://api.nedrex.net"
+
