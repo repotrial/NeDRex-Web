f32b77e0996c70249803b8d418ca4abcbc88ef5d
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2026-07-08 11:55:33 +0200

    updated some dependencies

--- web/frontend/src/Config.js
+++ web/frontend/src/Config.js
@@ -1,10 +1,7 @@
-// const HOST = "dev-api.web.nedrex.net"
-const HOST = "localhost"
-export const CONTEXT_PATH=""
-export const BACKEND_PORT= "8090"
-export const HOST_URL = "http://" + HOST + ":" + BACKEND_PORT
-export const SOCKET_URL = "http://" + HOST + ":8090"
-export const PATH_PREFIX= ""
-export const STATIC_PATH="/static/"
+export const HOST_URL = process.env.VUE_APP_HOST_URL || "http://localhost:8090"
+export const CONTEXT_PATH = process.env.VUE_APP_CONTEXT_PATH || ""
+export const SOCKET_URL = HOST_URL
+export const PATH_PREFIX = ""
+export const STATIC_PATH = "/static/"
 
 // export const NEDREX_API="https://apps.cosy.bio/licensed"
